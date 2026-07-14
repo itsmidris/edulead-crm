0783634ccb424fb89bae7b6145a4b135f16b643e
itsmidris <idrishiimran72@gmail.com>
2026-07-14 23:34:57 +0530

    JPA Audit Config

---
+++ src/main/java/com/itsmidris/edulead_crm/common/config/JpaAuditConfig.java
@@ -0,0 +1,9 @@
+package com.itsmidris.edulead_crm.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditConfig {
+}
