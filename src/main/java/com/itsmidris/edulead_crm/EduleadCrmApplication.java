200d8e070b8e586ebdda025387ecbd4a5fac584a
itsmidris <idrishiimran72@gmail.com>
2026-07-14 21:38:54 +0530

    EduLead CRM

---
+++ src/main/java/com/itsmidris/edulead_crm/EduleadCrmApplication.java
@@ -0,0 +1,13 @@
+package com.itsmidris.edulead_crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduleadCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduleadCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itsmidris/edulead_crm/EduleadCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsmidris.edulead_crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduleadCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
