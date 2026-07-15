36b5252a46c1740efc22028f14d9b81b090b4a53
itsmidris <idrishiimran72@gmail.com>
2026-07-15 09:25:42 +0530

    Generic Collection

---
+++ src/main/java/com/itsmidris/edulead_crm/common/payload/ApiResponse.java
@@ -0,0 +1,19 @@
+package com.itsmidris.edulead_crm.common.payload;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse <T>{
+    private boolean success;
+    private String message;
+    private T data;
+
+    @Builder.Default
+    private LocalDateTime timestamp = LocalDateTime.now();
+}
