2e5c7971587414bcfc380e3962cc8d63cc981b53
itsmidris <idrishiimran72@gmail.com>
2026-07-15 11:12:18 +0530

    Validation Exception

--- src/main/java/com/itsmidris/edulead_crm/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/itsmidris/edulead_crm/common/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import com.itsmidris.edulead_crm.common.payload.ErrorResponse;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -30,6 +31,24 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGenericException(Exception ex) {
         return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR,"Something Went Wrong, please try again later.");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidException(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ":" + error.getDefaultMessage())
+                .toList();
+
+        ErrorResponse response = ErrorResponse.builder()
+                .success(false)
+                .message("Validation failed")
+                .status(HttpStatus.BAD_REQUEST.value())
+                .errors(errors)
+                .build();
+
+        return ResponseEntity.badRequest().body(response);
     }
 
     private ResponseEntity<ErrorResponse> buildErrorResponse(HttpStatus status, String message) {
