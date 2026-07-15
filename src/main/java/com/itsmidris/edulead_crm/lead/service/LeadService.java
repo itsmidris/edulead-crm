bf6679312a59cc722c794e99fd52b7e34aa477cd
itsmidris <idrishiimran72@gmail.com>
2026-07-15 17:33:28 +0530

    Lead Service and Service Impl

---
+++ src/main/java/com/itsmidris/edulead_crm/lead/service/LeadService.java
@@ -0,0 +1,21 @@
+package com.itsmidris.edulead_crm.lead.service;
+
+import java.util.List;
+
+import com.itsmidris.edulead_crm.lead.dto.request.CreateLeadRequest;
+import com.itsmidris.edulead_crm.lead.dto.response.LeadResponse;
+import com.itsmidris.edulead_crm.lead.dto.response.LeadSummaryResponse;
+
+public interface LeadService {
+
+    LeadResponse createLead(CreateLeadRequest request);
+
+    LeadResponse getLeadById(Long id);
+
+    List<LeadResponse> getAllLeads();
+
+    List<LeadSummaryResponse> getActiveLeads();
+
+    void deactivateLead(Long id);
+
+}
