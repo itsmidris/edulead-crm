3d1a1a0dc4d5459a6007478e31610c3b4125a462
itsmidris <idrishiimran72@gmail.com>
2026-07-15 13:01:24 +0530

    Course Service

---
+++ src/main/java/com/itsmidris/edulead_crm/course/service/CourseService.java
@@ -0,0 +1,21 @@
+package com.itsmidris.edulead_crm.course.service;
+
+import com.itsmidris.edulead_crm.course.dto.request.CreateCourseRequest;
+import com.itsmidris.edulead_crm.course.dto.response.CourseResponse;
+import com.itsmidris.edulead_crm.course.dto.response.CourseSummaryResponse;
+
+import java.util.List;
+
+public interface CourseService {
+
+    CourseResponse createCourse(CreateCourseRequest request);
+
+    CourseResponse getCourseById(Long id);
+
+    List<CourseResponse> getAllCourses();
+
+    List<CourseSummaryResponse> getActiveCourses();
+
+    void disableCourse(Long id);
+
+}
