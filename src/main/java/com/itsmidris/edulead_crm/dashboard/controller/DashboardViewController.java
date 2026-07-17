a316b06c1cd7e08d1c56c700d3bf309a525745b8
itsmidris <idrishiimran72@gmail.com>
2026-07-17 17:11:05 +0530

    Thymeleaf Implementation

--- src/main/java/com/itsmidris/edulead_crm/dashboard/controller/DashboardViewController.java
+++ src/main/java/com/itsmidris/edulead_crm/dashboard/controller/DashboardViewController.java
@@ -1,13 +1,31 @@
 package com.itsmidris.edulead_crm.dashboard.controller;
 
+import com.itsmidris.edulead_crm.course.repository.CourseRepository;
+import com.itsmidris.edulead_crm.lead.repository.LeadRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashboardViewController {
 
+    private final LeadRepository leadRepository;
+    private final CourseRepository courseRepository;
+
+    public DashboardViewController(
+            LeadRepository leadRepository,
+            CourseRepository courseRepository) {
+
+        this.leadRepository = leadRepository;
+        this.courseRepository = courseRepository;
+    }
+
     @GetMapping("/")
-    public String dashboard() {
+    public String dashboard(Model model) {
+
+        model.addAttribute("leadCount", leadRepository.count());
+        model.addAttribute("courseCount", courseRepository.count());
+
         return "dashboard/dashboard";
     }
 }
